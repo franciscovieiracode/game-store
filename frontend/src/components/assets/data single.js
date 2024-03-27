5b07d59266cec75a8fc3ee17e3a2ed215517b2e7
DESKTOP-OH50SVV\franc <franciscovieiracode>
2024-03-27 19:14:47 +0000

    made home page responsive to mobile

--- frontend/src/components/assets/data single.js
+++ frontend/src/components/assets/data single.js
@@ -6,7 +6,7 @@ const data_single =
       "small_images":["https://gaming-cdn.com/images/products/9579/screenshot/tekken-8-pc-game-steam-wallpaper-1.jpg?v=1706550061","https://gaming-cdn.com/images/products/9579/screenshot/tekken-8-pc-game-steam-wallpaper-2.jpg?v=1706550061"],
       "new_price": 50.00,
       "old_price": 80.00,
-      "about":"Tekken 8 for PC is just the latest in a long line of Tekken games: it is the eighth main game, and tenth release overall. But it is only the second game to be made available on PC: the first being Tekken 7. Tekken (the word comes from the Japanese Tetsu no ken, meaning ‘Iron Fist,’) is a fighting game, and in this game, the developers have perfected their understanding of what their players want from the game.",
+      "about":"Tekken 8 for PC is just the latest in a long line of Tekken games: it is the eighth main game, and tenth release overall. But it is only the second game to be made available on PC: the first being Tekken 7. Tekken.",
       "review_stars": 4,
       "console":["PC", "XBOX", "PLAYSTATION"],
       "stock": true,
@@ -15,4 +15,5 @@ const data_single =
       "release_date":" 26 January 2024",
       "genre": ["Single-Player","Action"]
     }
+
   export default data_single
--- frontend/src/pages/GameDetail.jsx
+++ frontend/src/pages/GameDetail.jsx
@@ -1,38 +1,59 @@
-import React from 'react'
-import data_game from '../components/assets/data single'
-import '../pages/css/GameDetail.css'
+import React, { useState } from 'react';
+import data_game from '../components/assets/data single';
+import '../pages/css/GameDetail.css';
+import heart from '../components/assets/heart.png';
 
 export const GameDetail = () => {
+  const [selectedConsole, setSelectedConsole] = useState('');
+
+  const handleConsoleChange = (event) => {
+    setSelectedConsole(event.target.value);
+  };
+
+  const teste = () => {
+    alert(`Added to cart for ${selectedConsole}`);
+  };
+
   return (
     <div className='game-detail'>
       <div className="left">
         <div className="big-image">
-          <img src={data_game.image} height={500} alt="Game Image" />
+          <img src={data_game.small_images[0]} alt="Game Image" />
         </div>
         <div className="small-image">
-          <img src={data_game.small_images[0]} height={106} alt="" />
-          <img src={data_game.small_images[1]} height={106} alt="" />
+          <img src={data_game.small_images[0]} alt="" />
+          <img src={data_game.small_images[0]} alt="" />
+          <img src={data_game.small_images[1]} alt="" />
         </div>
         <div className="description">
+          <h2>ABOUT</h2>
           <p>{data_game.about}</p>
         </div>
       </div>
       <div className="right">
-          <div className="info">
-            {data_game.name}
-          </div>
-          <div className="stock">
-            <p>{data_game.stock}</p>
-            <p>{data_game.console}</p>
-          </div>
-          <div className="prices">
-            <p className='old-price'>{data_game.old_price}€</p>
-            <p className='new-price'>{data_game.new_price}€</p>
-          </div>
-          <div className="btn">
-            <button>Add to cart</button>
-          </div>
+        <div className="info">
+          {data_game.name}
+        </div>
+        <div className="console">
+          <select value={selectedConsole} onChange={handleConsoleChange}>
+            <option value="">Select Platform</option>
+            {data_game.console.map((console, index) => (
+              <option key={index} value={console}>
+                {console}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="prices">
+          <p className='percentage'>-30%</p>
+          <p className='old-price'>{data_game.old_price}€</p>
+          <p className='new-price'>{data_game.new_price}€</p>
+        </div>
+        <div className="btnfav">
+          <img src={heart} height={25} alt="" />
+          <button onClick={teste}>Add to cart</button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
